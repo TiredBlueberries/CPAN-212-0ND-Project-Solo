33e5d143b68226c0348fd2f143cc6b2799a772aa
TiredBlueberries <redesetgo684@gmail.com>
2025-04-17 13:27:25 -0400

    more small edits

--- group-project/saas-photo-editor/server/routers/canvasRouter.js
+++ group-project/saas-photo-editor/server/routers/canvasRouter.js
@@ -38,29 +38,4 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.put('/:id', async (req, res) => {
-  try {
-    const canvas = await Canvas.findOneAndUpdate(
-      { _id: req.params.id, user: req.user.userId },
-      req.body,
-      { new: true }
-    );
-    canvas ? res.json(canvas) : res.status(404).json({ message: 'Canvas not found' });
-  } catch (error) {
-    res.status(400).json({ message: 'Update failed' });
-  }
-});
-
-router.delete('/:id', async (req, res) => {
-  try {
-    const canvas = await Canvas.findOneAndDelete({
-      _id: req.params.id,
-      user: req.user.userId
-    });
-    canvas ? res.json({ message: 'Canvas deleted' }) : res.status(404).json({ message: 'Canvas not found' });
-  } catch (error) {
-    res.status(500).json({ message: 'Deletion failed' });
-  }
-});
-
 export default router;
